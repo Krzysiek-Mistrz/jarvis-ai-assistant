cf5a53dcabdd6b939e048a61abfe963d549e31a0
Krzysztof <krzyskuar@gmail.com>
2025-05-16 01:17:51 +0200

    changed api to google one and also changed def implementation of checking api key

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from google import genai
+from src.core import Jarvis
+
+def main():
+    api_key = input("Please set your GOOGLE_API_KEY: ")
+    while not api_key:
+        print("Error: Please set your GOOGLE_API_KEY environment variable.")
+        api_key = input("Please set your GOOGLE_API_KEY: ")
+
+    # Konfiguracja nowego klienta
+    client = genai.Client(api_key=api_key)
+
+    jarvis = Jarvis(api_key=api_key)
+    jarvis.talk()
+
+if __name__ == "__main__":
+    main()
